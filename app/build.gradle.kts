0c5442b5e41dc30c60ff6c0a81979a30b7981bae
Meghana-Merla <24je0654@iitism.ac.in>
2026-05-14 20:22:02 +0530

    Setup MVVM configuration and ViewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.meghana.hackhubx"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.meghana.hackhubx"
@@ -17,31 +13,73 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.appcompat)
+
     implementation(libs.material)
+
     implementation(libs.androidx.activity)
+
     implementation(libs.androidx.constraintlayout)
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-livedata-ktx:2.8.3"
+    )
+
+    implementation(
+        "androidx.navigation:navigation-fragment-ktx:2.8.0"
+    )
+
+    implementation(
+        "androidx.navigation:navigation-ui-ktx:2.8.0"
+    )
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    androidTestImplementation(
+        libs.androidx.espresso.core
+    )
+}
+
+kotlin {
+    jvmToolchain(17)
 }
